00fa8845a0c20db10f630e775c0e8aa18fb71232
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2024-10-04 22:50:01 +0400

    Added Angular Animations

--- Frontend/Frost/src/app/components/home/home.component.ts
+++ Frontend/Frost/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 
 @Component({
@@ -7,7 +9,15 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('slideDown', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('0.5s ease', style({ transform: 'translateY(0)', opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent {
 
