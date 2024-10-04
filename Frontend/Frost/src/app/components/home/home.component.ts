d1780ead5909a0103afbd84f1e8d794d6e8abd0f
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2024-10-04 18:21:06 +0400

    Co-authored-by: panko <abdur2k58@users.noreply.github.com>

--- Frontend/Frost/src/app/components/home/home.component.ts
+++ Frontend/Frost/src/app/components/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
 }
